6f132a1b038c4148b09304bbe058f476cd6b9d00
Kybox <nslr@riseup.net>
2017-12-11 15:10:31 +0100

    Ajout d'Apache Log4J2

--- src/fr/kybox/Mastermind.java
+++ src/fr/kybox/Mastermind.java
@@ -1,10 +1,20 @@
 package fr.kybox;
 
 import fr.kybox.game.Game;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class Mastermind {
 
+    private static Logger logger = LogManager.getLogger(Mastermind.class);
+
     public static void main(String[] args) {
+
+        logger.debug("msg de debug");
+        logger.info("msg d'info");
+        logger.warn("msg warning");
+        logger.error("msg error");
+        logger.fatal("msg fatal");
 
         System.out.println("Mastermind v1");
 
