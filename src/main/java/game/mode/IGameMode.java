76fcd69eac8c5fd6ae82bd9e936196cd161f4272
Kybox <nslr@riseup.net>
2017-12-20 15:55:06 +0100

    Ajout des commentaires de méthodes sur l'interface IGameMode

--- src/main/java/game/mode/IGameMode.java
+++ src/main/java/game/mode/IGameMode.java
@@ -2,6 +2,14 @@ package main.java.game.mode;
 
 public interface IGameMode {
 
+    /**
+     * Start the game after initializing the different parameters
+     */
     void startGame();
+
+    /**
+     * Performs a game round
+     * @return  True if the game is over otherwise false
+     */
     boolean gameTour();
 }
