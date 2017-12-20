38c5be63eacbf8f37d517e9d95b644ec08cab5ca
Kybox <nslr@riseup.net>
2017-12-20 15:43:38 +0100

    Ajout des commentaires de méthodes sur la classe Challenger

--- src/main/java/game/mode/Challenger.java
+++ src/main/java/game/mode/Challenger.java
@@ -1,8 +1,8 @@
 package main.java.game.mode;
 
 import main.java.controller.Controller;
-import main.java.game.Game;
 import main.java.game.GameOver;
+import main.java.game.player.ai.AI;
 import main.java.utils.SecretCode;
 import main.java.utils.Settings;
 import main.java.view.Display;
@@ -11,20 +11,28 @@ import java.util.Arrays;
 
 public class Challenger implements IGameMode {
 
-    private GameOver gameOver;
     private int[] secretCode;
-    private int maxTrials;
     private int trials;
     private boolean win;
     private boolean finished;
-    private Controller controller;
-    private boolean autoManagement;
 
+    private final AI computer;
+    private final int maxTrials;
+    private final GameOver gameOver;
+    private final Controller controller;
+    private final boolean autoManagement;
+
+    /**
+     * The Challenger constructor
+     * @param   gameOver        A GameOver object for the game
+     * @param   autoManagement  Indicates if the class manages itself the game rounds
+     */
     public Challenger(GameOver gameOver, boolean autoManagement){
 
         this.gameOver = gameOver;
         this.autoManagement = autoManagement;
 
+        computer = new AI();
         controller = new Controller();
         maxTrials = Settings.getTrials();
 
@@ -61,31 +69,38 @@ public class Challenger implements IGameMode {
             Display.info("------------------------");
             Display.info("Saisir une combinaison :");
 
-            String trial = controller.getGameInput();
+            String combination = controller.getGameInput();
 
-            if (SecretCode.isEqual(secretCode, trial)) {
+            if (SecretCode.isEqual(secretCode, combination)) {
                 win = true;
                 finished = true;
             }
             else {
-                Display.info("Réponse : " + SecretCode.getClues(Game.GAME_TYPE, secretCode, trial));
+                Display.info("Réponse : " + computer.getClues(secretCode, combination));
             }
         }
-
         return finished;
     }
 
-    public void setSecretCode(){
-
-        int nbBoxes = Settings.getBoxes();
-        int nbNumbers = Settings.getMaxNumbers();
-        secretCode = SecretCode.generate(nbBoxes, nbNumbers);
+    /**
+     * Generate a secret combinaition
+     */
+    private void setSecretCode(){
+        secretCode = SecretCode.generate(Settings.getBoxes(), Settings.getMaxNumbers());
     }
 
+    /**
+     * Retrieves the generated secret combination
+     * @return  The secret combination
+     */
     public int[] getSecretCode(){
         return secretCode;
     }
 
+    /**
+     * Retrieves the number of trials
+     * @return  Trials
+     */
     public int getTrials(){
         return trials;
     }
